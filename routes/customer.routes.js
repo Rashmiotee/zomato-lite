e1843c728a1b66671702f279092bffc05b3e9537
Rashmi_otee <rashmiotee55@gmail.com>
2025-01-15 09:39:24 +0530

    controller

--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -1,12 +1,14 @@
 // const { getLocation, updateCustomerInfo } = require("../controllers/customer.controllers")
 
-const { getLocation, updateCustomerInfo } = require("../controllers/customer.controller")
+const { getLocation, updateCustomerInfo, getResturants, getResturantMenu } = require("../controllers/customer.controller")
 
 const router = require("express").Router()
 
 router
     .post("/get-location", getLocation)
     .post("/update-info", updateCustomerInfo)
+    .get("/get-resturant", getResturants)
+    .get("/get-resturant-menu", getResturantMenu)
 
 
 module.exports = router
